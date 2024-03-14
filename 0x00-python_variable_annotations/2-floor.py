ff86b4a38ba9c98a36d5e62c5f81510eb7aeca1f
Sadson <sadsonmkumira@yahoo.com>
2024-03-14 11:57:55 +0000

    Adding file 2-floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""This is a module with a type-annotated function
+   floor which takes a float n as argument and returns
+   the floor of the float
+"""
+
+
+def floor(n: float) -> int:
+    """a function which takes a float and converts it into a floor integer"""
+    return int(n)
