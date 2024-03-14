619401227293745efa21cc0f09a536042c57a8f3
Sadson <sadsonmkumira@yahoo.com>
2024-03-14 11:49:32 +0000

    Adding files

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""a module that type-annotated function concat
+   that takes a string str1 and a string str2 as
+   arguments and returns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """a function that concatenates two strings"""
+    return f"{str1}{str2}"
