6bf2e70fd70207ce0e2ab4362233634d58eb7bc3
Sadson <sadsonmkumira@yahoo.com>
2024-03-18 14:00:21 +0000

    Adding file 4-tasks.py

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -4,14 +4,14 @@
 
 import asyncio
 from typing import List
-wait_n = __import__('1-concurrent_coroutines').wait_n
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """a function that utilizes wait_random from another module to return a
        list of random floating point numbers
     """
-    result = [wait_n(max_delay) for _ in range(n)]
+    result = [task_wait_random(max_delay) for _ in range(n)]
     tasks_delay = []
     for future_task in asyncio.as_completed(result):
         task_delay = await future_task
