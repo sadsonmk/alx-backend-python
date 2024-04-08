f6acd08e9fe624b0c1c1775016694463cc697cb9
Sadsonmk <sadsonmkumira@yahoo.com>
2024-04-08 05:49:58 -0700

    Adding file adjustments

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -19,9 +19,9 @@ class TestAccessNestedMap(unittest.TestCase):
     """
     @parameterized.expand(
             [
-                ({"a": 1}, ("a",)),
-                ({"a": {"b": 2}}, ("a",)),
-                ({"a": {"b": 2}}, ("a", "b")),
+                ({"a": 1}, ("a",), 1),
+                ({"a": {"b": 2}}, ("a",), {"b": 2}),
+                ({"a": {"b": 2}}, ("a", "b"), 2),
                 ]
             )
     def test_access_nested_map(self, nested_map: Mapping, path: Sequence, ep):
