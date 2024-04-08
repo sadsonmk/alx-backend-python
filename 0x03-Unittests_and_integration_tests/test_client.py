f8a8a71bbfd634a4cfacc1d39342099a18b3d82a
Sadsonmk <sadsonmkumira@yahoo.com>
2024-04-08 10:11:30 -0700

    Adding files

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,7 +2,7 @@
 """This is a module which implements tests for client.GithubOrgClient class"""
 
 import unittest
-from unittest import (Mock, patch, MagicMock)
+from unittest import (Mock, patch, MagicMock, PropertyMock)
 from parameterized import parameterized
 from typing import (
     List,
@@ -15,8 +15,8 @@ class TestGithubOrgClient(unittest.TestCase):
     """This class implements tests for test_org method"""
 
     @parameterized.expand([
-        ("google", {'login': "google"}),
-        ("abc", {'login': "abc"}),
+        ("google",),
+        ("abc",),
         ])
     @patch("client.get_json",)
     def test_org(self, org: str, result: Dict, func: MagicMock) -> None:
@@ -26,6 +26,19 @@ class TestGithubOrgClient(unittest.TestCase):
         self.assertEqual(org_client_obj.org(), result)
         func.assert_called_once_with(f"https://api.github.com/orgs/{org}")
 
+    def test_public_repos_url(self) -> None:
+        """Implement the test_public_repos_url method to unit-test
+            GithubOrgClient._public_repos_url
+        """
+        with patch.object(
+                GithubOrgClient.org,
+                "org",
+                ) as mock_obj:
+            res = {"repos_url": "https://api.github.com/orgs/test-org/repos"}
+            exp_url = res['repos_url']
+            mock_obj.return_value = res
 
-if __name__ == '__main__':
-    unittest.main()
+            client = GithubOrgClient("test-org")
+
+            public_repos_url = client._public_repos_url()
+            self.assertEqual(public_repos_url, exp_url)
