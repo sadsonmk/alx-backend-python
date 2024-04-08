485a88f62b9f71263a1fe687f638ad4d19e7aee3
Sadsonmk <sadsonmkumira@yahoo.com>
2024-04-08 09:14:11 -0700

    Adding files

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -13,16 +13,19 @@ from client import GithubOrgClient
 
 class TestGithubOrgClient(unittest.TestCase):
     """This class implements tests for test_org method"""
+
     @parameterized.expand([
         ("google", {'login': "google"}),
         ("abc", {'login': "abc"}),
         ])
-    @patch(
-            "client.get_json",
-            )
+    @patch("client.get_json",)
     def test_org(self, org: str, result: Dict, fn: MagicMock) -> None:
         """the method that tests the org function"""
         fn.return_value = MagicMock(return_value=result)
         org_client_obj = GithubOrgClient(org)
         self.assertEqual(org_client_obj.org(), result)
         fn.assert_called_once_with(f"https://api.github.com/orgs/{org}")
+
+
+if __name__ == '__main__':
+    unittest.main()
